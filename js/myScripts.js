08195a2bd8543c01fba4c0cf019c0bbf045da679
Igor Borisoglebski <igorborisoglebski@gmail.com>
2022-02-19 12:46:57 +0000

    adjusting responsive stickytopic top value

--- js/myScripts.js
+++ js/myScripts.js
@@ -4,6 +4,8 @@ removeHash();
 // Remove hash whenever hash is changed
 window.addEventListener('hashchange', removeHash, false);
 
+adjustStickyTitleTop();
+
 // When the user scrolls the page
 window.onscroll = function() {onScroll()};
 let heightSwitch = 550;
@@ -37,35 +39,43 @@ function onScroll() {
   }
   else {
     contactButton.className = "hide";
-  }
 
-  //topnavbar section
-  topnavbarsections = document.getElementsByClassName("topnavidx");
-  if (scrolled > 85){
-    current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace("active ", "");
-    topnavbarsections[3].className = "active topnavidx";
-  } 
-  else if (scrolled > 66){
-    current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace("active ", "");
-    topnavbarsections[2].className = "active topnavidx";
-  } 
-  else if (scrolled > 23){
-    current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace("active ", "");
-    topnavbarsections[1].className = "active topnavidx";
-  }
-  else {
-    current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace("active ", "");
-    topnavbarsections[0].className = "active topnavidx";
+    //topnavbar section
+    topnavbarsections = document.getElementsByClassName("topnavidx");
+    if (scrolled > 85){
+      current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace("active ", "");
+      topnavbarsections[3].className = "active topnavidx";
+    } 
+    else if (scrolled > 66){
+      current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace("active ", "");
+      topnavbarsections[2].className = "active topnavidx";
+    } 
+    else if (scrolled > 23){
+      current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace("active ", "");
+      topnavbarsections[1].className = "active topnavidx";
+    }
+    else {
+      current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace("active ", "");
+      topnavbarsections[0].className = "active topnavidx";
+    }
   }
 }
 
 // Remove hash and whatever comes after from the url
 function removeHash() {
   history.pushState(null, null, window.location.href.split("#")[0]);
+}
+
+function adjustStickyTitleTop() {
+  console.log("Adjusting Stikcy Topic Top");
+  var topnav = document.getElementById("topnav");
+  console.log("topnav.height=" + topnav.height);
+  var topnav_height = topnav.height;
+  document.getElementsByClassName("stickytitle").style["top"] = topnav_height;
 }
 
 console.log("Well, you seem to be very curious. So, you'll also get to know some of my personal interests and favorite activities: Fixing things, technology, motorcycle off-road riding, golf, reading, music and travelling.");
